d81623c06ab008f47e91087e93f609421fcbfeaf
SasiSekarP <109525298+SasiSekarP@users.noreply.github.com>
2022-09-21 21:36:17 +0530

    title change
    rock stone scissor is changed into rock paper scissor. result alert content also changed

--- script.js
+++ script.js
@@ -13,54 +13,54 @@ const choice = {
 play();
 function play() {
     // user input
-let userInput = Number(prompt('Enter a number 1 for Rock, 2 for Paper, 3 for Scissor'));
+    let userInput = Number(prompt('Enter a number 1 for Rock, 2 for Paper, 3 for Scissor'));
 
-// user choice
-let userChoice = choice[userInput];
+    // user choice
+    let userChoice = choice[userInput];
 
-// computer random choice
-let computerChoice = Random(choice);
-function Random(a) {
-    const key = Object.keys(a);
-    return a[key[Math.floor(Math.random()*key.length)]]
-};
-// comparing both
-function game (userChoice,computerChoice) {
-    if (userChoice === computerChoice) {
-        return 'tie';
-    } else if (userChoice === "Rock"){
-        if (computerChoice === "Paper") {
-            return "Computer won";
-        } else {
-            return "You won";
+    // computer random choice
+    let computerChoice = Random(choice);
+    function Random(a) {
+        const key = Object.keys(a);
+        return a[key[Math.floor(Math.random()*key.length)]]
+    };
+    // comparing both
+    function game (userChoice,computerChoice) {
+        if (userChoice === computerChoice) {
+            return 'tie';
+        } else if (userChoice === "Rock"){
+            if (computerChoice === "Paper") {
+                return "Computer won";
+            } else {
+                return "You won";
+            }
+        } else if (userChoice === "Paper"){
+            if (computerChoice === "Rock") {
+                return "You won";
+            } else {
+                return "Computer won";
+            }
+        } else if (userChoice === "Scissor"){
+            if (computerChoice === "Rock") {
+                return "Computer won";
+            } else {
+                return "You won";
+            }
         }
-    } else if (userChoice === "Paper"){
-        if (computerChoice === "Rock") {
-            return "You won";
-        } else {
-            return "Computer won";
-        }
-    } else if (userChoice === "Scissor"){
-        if (computerChoice === "Rock") {
-            return "Computer won";
-        } else {
-            return "You won";
-        }
-    }
-};
+    };
 
-// result
-let whoWon = game(userChoice, computerChoice);
-// proved result
+    // result
+    let whoWon = game(userChoice, computerChoice);
+    // proved result
 
-let shouldWeContinue = result();
+    let shouldWeContinue = result();
 
     function result() {
         if (whoWon === undefined) {
-            alert("You skiped the game!!!");
+            alert(`You skiped the game!!!`);
 
         } else {
-            let a = confirm(whoWon + " !. Do you want to continue?");
+            let a = confirm(`You : ${userChoice} , Computer : ${computerChoice}. So, ${whoWon} !. Do you want to continue ?`);
             return a;
         }
     }
